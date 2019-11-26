bc795f903c397b53cf519382171f41cfc1709619
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-11-26 09:10:50 -0700

    Update spread.spec.ts

--- week4/src/spread.spec.ts
+++ week4/src/spread.spec.ts
@@ -1,28 +1,24 @@
 import { expect } from 'chai';
 import 'mocha';
-import { newNumbers } from './spread';
-import { newContenders } from './spread';
-import { newPersons } from './spread';
-import { newUpdates } from './spread';
+import { newNumbers, originalNumbers } from './spread';
+import { newContenders, originalContenders } from './spread';
+import { newPersons, originalPersons } from './spread';
+import { newUpdates, originalUpdates, update, data } from './spread';
 
 describe('Test for spread.ts', () => {
     it("should output array [1,2,3,4,5]", () => {
-        expect(newNumbers).to.be.eql([1,2,3,4,5])
+        expect(newNumbers).to.be.eql(originalNumbers)
     });
 
-    it("should output array [ 'first', 'first', 'second', 'third', 'fourth', 'fifth' ]", () => {
-        expect(newContenders).to.be.eql([ 'first', 'first', 'second', 'third', 'fourth', 'fifth' ])
+    it("should output array [ ['first'], 'first', 'second', 'third', 'fourth', 'fifth' ]", () => {
+        expect(newContenders.length).to.be.eql(originalContenders + 1)
     });
 
     it("should output objects { man: 'Bob', boy: 'Kyle' }", () => {
-        expect(newPersons).to.be.equal({ man: 'Bob', boy: 'Kyle' })
+        expect(newPersons).to.be.eql(originalPersons)
     });
 
     it("should output values [{ id: 0, task: 'Do the thing' },{ id: 1, task: 'Do the other thing... again' },{ id: 2, task: 'Do the last thing'}]", () => {      
-        expect(newUpdates).to.be.eql([
-            { id: 0, task: 'Do the thing' },
-            { id: 1, task: 'Do the other thing... again' },
-            { id: 2, task: 'Do the last thing' }
-          ])
+        expect(newUpdates(update,...data)).to.be.eql(originalUpdates)
     });
 });
