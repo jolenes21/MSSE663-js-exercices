a25b191beae2cd7e0d390718ea189763a3b93a2b
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-11-30 18:01:53 -0700

    Update array-destructuring.spec.ts

--- week5/src/array-destructuring.spec.ts
+++ week5/src/array-destructuring.spec.ts
@@ -1,17 +1,19 @@
-import { expect } from 'chai';
-import 'mocha';
+import { expect } from "chai";
+import "mocha";
 import { fruits, food, welcome } from "./array-destructuring";
 
-describe('Array destructuring tests', () => {
-  it('#1 should have a variable for every element in the given array', () => {
-    expect(fruits.length).to.be.eql(['apple','banana'].length)
+describe("Array destructuring tests", () => {
+  it("#1 should have a variable for every element in the given array", () => {
+    expect(fruits.length).to.be.eql(["apple", "banana"].length);
   });
 
-  it('#2 should have a variable for the first 2 elements then spread the rest', () => {
-    expect(food.length).to.be.eql(['apple','banana','chocolate', 'pears', 'oats', 'pizza'].length)
+  it("#2 should have a variable for the first 2 elements then spread the rest", () => {
+    expect(food.length).to.be.eql(
+      ["apple", "banana", "chocolate", "pears", "oats", "pizza"].length
+    );
   });
 
-  it('#3 should construct a statement from the given objarrayect', () => {
-    expect(welcome).to.be.eql(['hello', 'taylor'])
+  it("#3 should construct a statement from the given objarrayect", () => {
+    expect(welcome).to.be.eql(["hello", "taylor"]);
   });
 });
