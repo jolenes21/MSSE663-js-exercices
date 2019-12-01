f35b6c7542aff3e0d485843efebb9a1e721c065a
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-11-30 18:03:57 -0700

    Update object-destructuring.ts

--- week5/src/object-destructuring.ts
+++ week5/src/object-destructuring.ts
@@ -1,5 +1,5 @@
 // #1 Object Destructuring
-export const object = { name: 'elvis', title: 'hip swinger' };
+export const object = { name: "elvis", title: "hip swinger" };
 
 // deconstruct here
 
@@ -10,10 +10,10 @@ console.log(job); // 'hip swinger'
 
 // #2 Object Matching: Nested destructuring
 export const nestedObject = {
-  user: 'elvis',
+  user: "elvis",
   address: {
-    city: 'denver',
-    state: 'colorado'
+    city: "denver",
+    state: "colorado"
   },
   id: 1
 };
@@ -30,15 +30,15 @@ console.log(id); // 1
 
 // reconstruct here
 
-export const newObject = { user, address, id};
+export const newObject = { user, address, id };
 
 console.log(newObject);
 
 // #3 Object with key value pairs: construct a statement
-export const welcome = { greeting: 'hello', name: 'taylor' };
+export const welcome = { greeting: "hello", name: "taylor" };
 
 let { greeting: greeting, name: name } = welcome;
 
-console.log(greeting, ',', name, '!');
+console.log(greeting, ",", name, "!");
 
 // 'hello, taylor!'
