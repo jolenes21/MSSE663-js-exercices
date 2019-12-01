d3795b1929e6c6f0c77886127a6fa3b00fbcaa77
jolenes21 <56704640+jolenes21@users.noreply.github.com>
2019-11-30 18:02:20 -0700

    Update array-destructuring.ts

--- week5/src/array-destructuring.ts
+++ week5/src/array-destructuring.ts
@@ -1,5 +1,5 @@
 // #1 Variable swapping
-export const fruits = ['apple', 'banana'];
+export const fruits = ["apple", "banana"];
 
 // Currently, I would get the following returns:
 /* console.log(a); // 'apple'
@@ -7,27 +7,27 @@ console.log(b); // 'banana' */
 
 // deconstruct here
 
-let [b,a] = fruits;
+let [b, a] = fruits;
 
 // Assign variables using ES6 so that we get (note, you cannot just make a completely new array):
 console.log(a); // 'banana';
 console.log(b); // 'apple';
 
 // #2 Variables and Rest
-export const food = ['apple', 'banana', 'chocolate', 'pears', 'oats', 'pizza'];
+export const food = ["apple", "banana", "chocolate", "pears", "oats", "pizza"];
 
 // deconstruct here
 
-let [c,d, ...others] = food;
+let [c, d, ...others] = food;
 
 console.log(c); // 'apple' <<--JSL: a and b conflicted with first exercise, so I updated letters
 console.log(d); // 'banana'
 console.log(others); // ['chocolate, 'pears', 'oats', 'pizza']
 
 // #3 Array: Contruct a statement
-export const welcome = ['hello', 'taylor'];
+export const welcome = ["hello", "taylor"];
 let [greeting, name] = welcome;
 
-console.log(greeting, ', ', name, '!');
+console.log(greeting, ", ", name, "!");
 
 // 'hello, taylor!'
