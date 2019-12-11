de684dd0a2b3b821e79a25237f36dbb93aab44b5
jolenes21 <jolenesantos@gmail.com>
2019-12-10 18:13:26 -0700

    some HOF

--- week6/src/higher-order-funcs.spec.ts
+++ week6/src/higher-order-funcs.spec.ts
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
 import 'mocha';
+import { allThings } from './higher-order-funcs'
 
 describe('Higher Order Function tests', () => {
-  it("#1 should produce the expected result `[{ id: 1, name 'thing' }, { id: 2, name 'thing' }]` after hoisting", () => {});
+  it("#1 should produce the expected result `[{ id: 1, name 'thing' }, { id: 2, name 'thing' }]` after hoisting", () => {
+    expect(allThings).to.be.eql(['thing1', 'thing2'])
+  });
 
-  it('#2 should return a single thing from array of things objects after hoisting and currying', () => {});
+  it('#2 should return a single thing from array of things objects after hoisting and currying', () => {
+    
+  });
 });
--- week6/src/higher-order-funcs.ts
+++ week6/src/higher-order-funcs.ts
@@ -8,7 +8,34 @@
  * - Then Hoist the callback function.
  *
  */
-const things = ['thing1', 'thing2'];
+
+export const Thing = [
+    { id: 1, name: 'thing'},
+    { id: 2, name: 'thing'}
+]
+
+export interface Things {
+    id: any,
+    name: string
+}
+
+export function allThings() {
+
+}
+
+//I want to be able to map id to name, return things = concat () {name, id}
+
+export const things = ['thing1', 'thing2'];
+
+/* example callback
+
+function functionOne(x) { return x; };
+
+function functionTwo(var1) {
+    // some code
+}
+
+functionTwo(functionOne); */
 
 /**
  * #2 Higher order then curry.
@@ -17,3 +44,5 @@ const things = ['thing1', 'thing2'];
  * - Filter the results from 1 to return a single thing object.
  * - Hoist that filter callback and curry all functions.
  */
+
+type thingFunction = { [key: string]: string }
